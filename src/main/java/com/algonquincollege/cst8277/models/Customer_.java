68d246f123152cb4893f10f617e82ef55664b508
Svetlana <netc0004@algonquinlive.com>
2019-04-08 17:00:56 +0000

    added M:M relationships for Cart and Product

--- src/main/java/com/algonquincollege/cst8277/models/Customer_.java
+++ src/main/java/com/algonquincollege/cst8277/models/Customer_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2019-04-07T04:25:59.373+0000")
+@Generated(value="Dali", date="2019-04-08T15:32:00.240+0000")
 @StaticMetamodel(Customer.class)
 public class Customer_ extends ModelBase_ {
 	public static volatile SingularAttribute<Customer, String> firstName;
