042bc1ed3769fd47f0645b6f7e4e259f0c2b2e86
Anshtein <anshtein@yahoo.com>
2019-04-08 13:03:55 -0400

    Customer-Contact 1:1 added Minor fixes in persistence

--- src/main/java/com/algonquincollege/cst8277/models/Contact_.java
+++ src/main/java/com/algonquincollege/cst8277/models/Contact_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2019-04-07T04:42:19.603+0000")
+@Generated(value="Dali", date="2019-04-08T16:29:11.670+0000")
 @StaticMetamodel(Contact.class)
 public class Contact_ extends ModelBase_ {
 	public static volatile SingularAttribute<Contact, String> email;
@@ -13,4 +13,5 @@ public class Contact_ extends ModelBase_ {
 	public static volatile SingularAttribute<Contact, String> city;
 	public static volatile SingularAttribute<Contact, String> province;
 	public static volatile SingularAttribute<Contact, String> postalCode;
+	public static volatile SingularAttribute<Contact, Customer> customer;
 }
