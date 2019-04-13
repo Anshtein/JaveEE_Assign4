8300d5bf083b52080581f066eb373550291b0333
Anshtein <anshtein@yahoo.com>
2019-04-13 00:27:49 -0400

    When will it work?

---
+++ src/main/java/com/algonquincollege/cst8277/models/Product_.java
@@ -0,0 +1,15 @@
+package com.algonquincollege.cst8277.models;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2019-04-13T02:20:51.992+0000")
+@StaticMetamodel(Product.class)
+public class Product_ extends ModelBase_ {
+	public static volatile SingularAttribute<Product, Choice> choice;
+	public static volatile ListAttribute<Product, Category> categories;
+	public static volatile SingularAttribute<Product, String> name;
+	public static volatile SingularAttribute<Product, Double> price;
+}
