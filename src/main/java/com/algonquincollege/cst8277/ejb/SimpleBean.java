40b55c2c745a7c2a8dd0268c412e08ddee18a581
Ksenia <hgdryuk>
2019-04-14 19:52:17 +0000

    comments

--- src/main/java/com/algonquincollege/cst8277/ejb/SimpleBean.java
+++ src/main/java/com/algonquincollege/cst8277/ejb/SimpleBean.java
@@ -1,3 +1,15 @@
+/********************************************************************egg***m******a**************n************
+ * File: SimpleBean.java
+ * Course materials (19W) CST 8277
+ * @author Mike Norman
+ * @author Elena Soukhanov 040871451
+ * @author Ksenia Lopukhina 040892102
+ * @author Svetlana Netchaeva 040858724
+ * @author Anna Shteyngart 040883547
+ * @author Pavel Jilinski 040878295
+ * @date 2019 04
+ *
+ */
 package com.algonquincollege.cst8277.ejb;
 
 import static com.algonquincollege.cst8277.utils.RestDemoConstants.PU_NAME;
@@ -21,16 +33,28 @@ import com.algonquincollege.cst8277.models.PlatformRole_;
 import com.algonquincollege.cst8277.models.PlatformUser;
 
 
-
+/**
+ * Stateless session bean containing business logic associated with Customer entity 
+ */
 @Stateless
 public class SimpleBean {
 
+    /**
+     * injects BuildUser bean
+     */
     @Inject
     protected BuildUser buildUser;
     
+    /**
+     * EntityManager injected into the bean
+     */
     @PersistenceContext(unitName = PU_NAME)
     protected EntityManager em;
 
+    /**
+     * finds the list of all customers
+     * @return List of Customer
+     */
     public List<Customer> getCustomerList() {
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Customer> cq = cb.createQuery(Customer.class);
@@ -38,11 +62,21 @@ public class SimpleBean {
         return em.createQuery(cq).getResultList();
     }
 
-
+    /**
+     *  finds a customer by id
+     * @param id
+     * @return Customer customer
+     */
     public Customer getCustomerById(int id) {
         return em.find(Customer.class, id);
     }
     
+    /**
+     * checks if username of a customer found is equal to the one passed in parameters
+     * @param username
+     * @param id
+     * @return true if it is equal, false otherwise
+     */
     public boolean checkCustomerUsernameId(String username, int id) {
         Customer customer = getCustomerById(id);
         PlatformUser pu = customer.getUser();
@@ -54,6 +88,12 @@ public class SimpleBean {
         
     }
     
+    /**
+     * adds a new customer
+     * @param firstName
+     * @param lastName
+     * @return true if customer was added, false otherwise
+     */
     public boolean addCustomer(String firstName, String lastName) {
         if (!firstName.isEmpty() && !lastName.isEmpty()) {
             String userPassword = "temppwd";
@@ -84,6 +124,11 @@ public class SimpleBean {
         return false;
     }
     
+    /**
+     * deletes customer
+     * @param customer
+     * @return true if customer was deleted, false otherwise
+     */
     public boolean deleteCustomer(Customer customer) {
         if (!em.contains(customer)) {
             customer = em.merge(customer);
@@ -95,6 +140,13 @@ public class SimpleBean {
         else return true;   
     }
     
+    /**
+     * updates Customer
+     * annotated with @TransactionAttribute to specify 
+     * whether the container is to invoke a business method within a transaction context
+     * @param customerWithUpdatedFields
+     * @return Customer customer
+     */
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public Customer updateCustomer(Customer customerWithUpdatedFields) {
         em.merge(customerWithUpdatedFields);
--- src/main/java/com/algonquincollege/cst8277/models/ModelBase.java
+++ src/main/java/com/algonquincollege/cst8277/models/ModelBase.java
@@ -2,7 +2,12 @@
  * File: ModelBase.java
  * Course materials (19W) CST 8277
  * @author Mike Norman
- * @date 2019 03
+ * @author Elena Soukhanov 040871451
+ * @author Ksenia Lopukhina 040892102
+ * @author Svetlana Netchaeva 040858724
+ * @author Anna Shteyngart 040883547
+ * @author Pavel Jilinski 040878295
+ * @date 2019 04
  *
  */
 package com.algonquincollege.cst8277.models;
@@ -15,13 +20,28 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Version;
 
+/**
+ * Abstract class that is base of (class) hierarchy for all com.algonquincollege.cst8277.models @Entity classes
+ */
 @MappedSuperclass
 public abstract class ModelBase implements Auditable {
 
+    /**
+     * id of an entity
+     */
     protected int id;
+    /**
+     * version of an entity
+     */
     protected int version;
+    /**
+     * Audit object inherited by an entity
+     */
     protected Audit audit = new Audit();
 
+    /**
+     * no-args constructor
+     */
     public ModelBase() {
         super();
         this.audit.setCreatedDate(LocalDateTime.now());
@@ -29,26 +49,49 @@ public abstract class ModelBase implements Auditable {
         this.audit.setUpdatedDate(this.audit.getCreatedDate());
     }
 
+    /**
+     * gets auto-generated id of an entity
+     * @return int id
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int getId() {
         return id;
     }
+    /**
+     * sets id of an entity
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * gets verstion of an entity
+     * @return int version
+     */
     @Version
     public int getVersion() {
         return version;
     }
+    /**
+     * sets verstion of an entity
+     * @param version
+     */
     public void setVersion(int version) {
         this.version = version;
     }
 
+    /**
+     * gets Audit object
+     * return Audit audit
+     */
     public Audit getAudit() {
         return audit;
     }
+    /**
+     * sets Audit object
+     */
     public void setAudit(Audit audit) {
         this.audit = audit;
     }
